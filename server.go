fb52207d377f29410da9307d3a6e5295d0fe84ce
mi11km <mi11km.basket@gmail.com>
2020-12-07 22:18:44 +0900

    go get github.com/rs/cors and github.com/go-chi/chi, and implement middleware

--- server.go
+++ server.go
@@ -7,9 +7,13 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/mi11km/zikanwarikun-back/graph"
 	"github.com/mi11km/zikanwarikun-back/graph/generated"
 	database "github.com/mi11km/zikanwarikun-back/internal/db"
+	"github.com/mi11km/zikanwarikun-back/internal/middleware/auth"
+	"github.com/rs/cors"
 )
 
 // todo configファイルから読み込む
@@ -29,11 +33,21 @@ func main() {
 		}
 	}()
 
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+	router.Use(auth.Middleware())
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }

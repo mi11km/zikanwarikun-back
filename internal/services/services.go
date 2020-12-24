2e5696ecbabe0f9bf49b40f67267f9ce8213592f
mi11km <mi11km.basket@gmail.com>
2020-12-25 02:14:39 +0900

    change user service a little

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,15 +1,20 @@
 package services
 
 import (
-	"context"
-
 	"github.com/mi11km/zikanwarikun-back/graph/model"
+	"github.com/mi11km/zikanwarikun-back/internal/db/models/users"
 )
 
 type UserService interface {
 	CreateUser(input model.NewUser) (string, error)
-	UpdateUser(input model.UpdateUser, ctx context.Context) (string, error)
-	DeleteUser(input model.DeleteUser, ctx context.Context) (bool, error)
+	UpdateUser(input *model.UpdateUser, u users.User) (string, error)
+	DeleteUser(input model.DeleteUser, u users.User) (bool, error)
 	Login(input model.Login) (string, error)
-	RefreshToken(input model.RefreshTokenInput) (string, error)
+	RefreshToken(token string) (string, error)
+}
+
+type TimetableService interface {
+	CreateTimetable(input model.NewTimetable) (*model.Timetable, error)
+	UpdateTimetable(input model.UpdateTimetable) (*model.Timetable, error)
+	DeleteTimetable(input int) (bool, error)
 }

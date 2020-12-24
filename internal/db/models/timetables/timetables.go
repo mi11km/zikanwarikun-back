ca0a0305e81b7da50a86205652c4fa7f508805ef
mi11km <mi11km.basket@gmail.com>
2020-12-25 02:17:38 +0900

    add timetable service to resolvers

--- internal/db/models/timetables/timetables.go
+++ internal/db/models/timetables/timetables.go
@@ -1,6 +1,9 @@
 package timetables
 
-import "github.com/mi11km/zikanwarikun-back/internal/db/models/users"
+import (
+	"github.com/mi11km/zikanwarikun-back/graph/model"
+	"github.com/mi11km/zikanwarikun-back/internal/db/models/users"
+)
 
 type Timetable struct {
 	ID           int         `json:"id"`
@@ -11,4 +14,16 @@ type Timetable struct {
 	CreatedAt    string      `json:"created_at"`
 	UpdatedAt    string      `json:"updated_at"`
 	User         *users.User `json:"user"`
+}
+
+func (t *Timetable) CreateTimetable(input model.NewTimetable) (*model.Timetable, error) {
+	return nil, nil
+}
+
+func (t *Timetable) UpdateTimetable(input model.UpdateTimetable) (*model.Timetable, error) {
+	return nil, nil
+}
+
+func (t *Timetable) DeleteTimetable(input int) (bool, error) {
+	return false, nil
 }

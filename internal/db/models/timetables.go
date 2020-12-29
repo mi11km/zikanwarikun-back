40aaf1761561caa6d556b7e2b57d4c71513322a5
mi11km <mi11km.basket@gmail.com>
2020-12-30 00:44:45 +0900

    implement API around timetables

--- internal/db/models/timetables.go
+++ internal/db/models/timetables.go
@@ -8,6 +8,7 @@ import (
 	"github.com/mi11km/zikanwarikun-back/graph/model"
 	database "github.com/mi11km/zikanwarikun-back/internal/db"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type Timetable struct {
@@ -21,45 +22,31 @@ type Timetable struct {
 	Classes      []*Class
 }
 
-/* CreateTimetable 時間割を新しくDBに作成してそれをデフォルトにする */
-func (t *Timetable) CreateTimetable(input model.NewTimetable, user User) (*model.Timetable, error) {
-	// todo userのtimetableの中でtrueになっているものをfalseにする
-	//database.Db.Model(&Timetable{}).Where("is_default = ?", true).Update("is_default", false)
+const Association = "Timetables"
 
-	newTimetable := &Timetable{
-		Name:         input.Name,
-		ClassDays:    input.Days,
-		ClassPeriods: input.Periods,
-		IsDefault:    true,
-		Users:        []User{user},
-	}
-	result := database.Db.Create(newTimetable)
-	if result.Error != nil {
-		log.Printf("action=create timetable, status=failed, err=%s", result.Error)
-		return nil, result.Error
-	}
-
-	dbTimetables, err := FetchTimetablesByUserId(user.ID)
+/* Create 時間割を新しくDBに作成してis_default=trueとなる時間割を新しいやつ１つにする */
+func (t *Timetable) Create(input model.NewTimetable, user User) error {
+	err := ChangeIsDefaultToFalse(user)
 	if err != nil {
-		log.Printf("action=create timetable, status=failed, err=%s", err)
-		return nil, err
+		log.Printf("action=create timetable data, status=failed, err=%s", err)
+		return err
 	}
-	graphUser := &model.User{
-		ID:     user.ID,
-		Email:  user.Email,
-		School: user.School,
-		Name:   user.Name,
+
+	t.Name = input.Name
+	t.ClassDays = input.Days
+	t.ClassPeriods = input.Periods
+	t.IsDefault = true
+
+	if err := database.Db.Model(&user).Association(Association).Append(t); err != nil {
+		log.Printf("action=create timetable data, status=failed, err=%s", err)
+		return err
 	}
-	graphTimetables := ConvertTimetablesFromDbToGraph(dbTimetables, graphUser)
-	graphUser.Timetables = graphTimetables
 
-	graphTimetable := ConvertTimetableFromDbToGraph(newTimetable, graphUser)
-
-	log.Printf("action=create timetable, status=success")
-	return graphTimetable, nil
+	log.Printf("action=create timetable data, status=success")
+	return nil
 }
 
-func (t *Timetable) UpdateTimetable(input model.UpdateTimetable, user User) (*model.Timetable, error) {
+func (t *Timetable) Update(input model.UpdateTimetable, user User) error {
 	updateData := make(map[string]interface{})
 	if input.Name != nil {
 		updateData["name"] = *input.Name
@@ -73,106 +60,86 @@ func (t *Timetable) UpdateTimetable(input model.UpdateTimetable, user User) (*mo
 	if input.IsDefault != nil {
 		updateData["is_default"] = *input.IsDefault
 		if *input.IsDefault {
-			database.Db.Model(&Timetable{}).
-				Where("is_default = ?", true).Update("is_default", false)
+			err := ChangeIsDefaultToFalse(user)
+			if err != nil {
+				log.Printf("action=update timetable data, status=failed, err=%s", err)
+				return err
+			}
 		}
 	}
 	if len(updateData) == 0 {
-		log.Printf("action=update dbTimetable, status=failed, err=update data is not set")
-		return nil, fmt.Errorf("update data must be set")
+		log.Printf("action=update timetable data, status=failed, err=update data is not set")
+		return fmt.Errorf("update data must be set")
 	}
 
-	id, err := strconv.Atoi(input.ID)
-	if err != nil {
-		log.Printf("action=update timetable, status=failed, err=%s", err)
-		return nil, err
+	if err := database.Db.Model(t).Updates(updateData).Error; err != nil {
+		log.Printf("action=update timetable data, status=failed, err=%s", err)
+		return err
 	}
-	dbTimetable := new(Timetable)
-	dbTimetable.ID = uint(id)
 
-	result := database.Db.Model(dbTimetable).Updates(updateData)
-	if result.Error != nil {
-		log.Printf("action=update timetable, status=failed, err=%s", result.Error)
-		return nil, result.Error
-	}
-	graphUser := &model.User{
-		ID:     user.ID,
-		Email:  user.Email,
-		School: user.School,
-		Name:   user.Name,
-	}
-	graphTimetable := ConvertTimetableFromDbToGraph(dbTimetable, graphUser)
-
-	// todo? userにtimetablesも入れとくべきか。入れても使わない気がする
-
-	log.Printf("action=update timetable, status=success")
-	return graphTimetable, nil // todo? updateしたデータとidとupdatedAt以外空になってる。
+	log.Printf("action=update timetable data, status=success")
+	return nil
 }
 
-// todo 関連レコードも一括削除する
-func (t *Timetable) DeleteTimetable(input string) (bool, error) {
+func (t *Timetable) Delete(input string) (bool, error) {
 	id, err := strconv.Atoi(input)
 	if err != nil {
-		log.Printf("action=delete timetable, status=failed, err=%s", err)
+		log.Printf("action=delete timetable data, status=failed, err=%s", err)
 		return false, err
 	}
-	dbTimetable := new(Timetable)
-	dbTimetable.ID = uint(id)
+	t.ID = uint(id)
 
-	result := database.Db.Delete(dbTimetable)
-	if result.Error != nil {
-		log.Printf("action=delete timetable, status=failed, err=%s", result.Error)
-		return false, result.Error
+	if err := database.Db.
+		Select("ClassTime", "Class", clause.Associations).Delete(t).Error; err != nil {
+		log.Printf("action=delete timetable data, status=failed, err=%s", err)
+		return false, err
 	}
 
-	log.Printf("action=delete timetable, status=success")
+	log.Printf("action=delete timetable data, status=success")
 	return true, nil
 }
 
-/* FetchTimetablesByUserId user_idからdbの時間割データを全て取得する */
-func FetchTimetablesByUserId(userId string) ([]*Timetable, error) {
-	var timetables []*Timetable
-	result := database.Db.Where("user_id = ?", userId).Find(&timetables)
-	if result.Error != nil {
-		return nil, result.Error
+func FetchTimetableById(id string) *Timetable {
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		log.Printf("action=fetch timetable by id, status=failed, err=%s", err)
+		return nil
 	}
-	return timetables, nil
+	timetable := &Timetable{}
+	timetable.ID = uint(i)
+	database.Db.First(timetable)
+	return timetable
 }
 
-/* FetchDefaultTimetableByUserId user_idからdbのデフォルトがtrueになってる時間割データを取得する */
-func FetchDefaultTimetableByUserId(userId string) (*Timetable, error) {
+/* FetchDefaultTimetableByUserId userからis_defaultがtrueになってる時間割データを取得する(最初の一件分) */
+func FetchDefaultTimetableByUser(user User) (*Timetable, error) {
 	var defaultTimetable Timetable
-	result := database.Db.Where("user_id = ? AND is_default = ?", userId, true).First(&defaultTimetable)
-	if result.Error != nil {
-		return nil, result.Error
+	if err := database.Db.Model(&user).Where("is_default = ?", true).
+		Association(Association).Find(&defaultTimetable); err != nil {
+		return nil, err
 	}
 	return &defaultTimetable, nil
 }
 
-/* ConvertTimetableFromDbToGraph １つ、dbの時間割データをgraphql用のモデルに変換する */
-func ConvertTimetableFromDbToGraph(dbTimetable *Timetable, user *model.User) *model.Timetable {
-	graphTimetable := &model.Timetable{
-		ID:        strconv.Itoa(int(dbTimetable.ID)),
-		Name:      dbTimetable.Name,
-		Days:      dbTimetable.ClassDays,
-		Periods:   dbTimetable.ClassPeriods,
-		CreatedAt: dbTimetable.CreatedAt.String(),
-		UpdatedAt: dbTimetable.UpdatedAt.String(),
-		IsDefault: dbTimetable.IsDefault,
-		Classes:   GetGraphClasses(int(dbTimetable.ID)),
-		//Classtimes: , // todo
-		//RowData: ,
-		User: user,
+/* FetchTimetablesByUser userの時間割データを全て取得する */
+func FetchTimetablesByUser(user User) ([]*Timetable, error) {
+	var timetables []*Timetable
+	if err := database.Db.Model(&user).Association(Association).Find(&timetables); err != nil {
+		return nil, err
 	}
-	return graphTimetable
+	return timetables, nil
 }
 
-/* ConvertTimetablesFromDbToGraph 複数のdbの時間割データをgraphql用のモデルに変換する */
-func ConvertTimetablesFromDbToGraph(dbTimetables []*Timetable, user *model.User) []*model.Timetable {
-	var graphTimetables []*model.Timetable
-	for _, dbTimetable := range dbTimetables {
-		graphTimetable := ConvertTimetableFromDbToGraph(dbTimetable, user)
-		graphTimetables = append(graphTimetables, graphTimetable)
+/* userのtimetableの中でis_defaultがtrueになっているものをfalseにする */
+func ChangeIsDefaultToFalse(user User) error {
+	timetables, err := FetchTimetablesByUser(user)
+	if err == nil {
+		for _, t := range timetables {
+			if t.IsDefault == true {
+				database.Db.Model(t).Updates(map[string]interface{}{"is_default": false})
+			}
+		}
+		return nil
 	}
-	return graphTimetables
+	return err
 }
